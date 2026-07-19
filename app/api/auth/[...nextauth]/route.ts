1d9e679de63de557c6318b2291b460622e2c922e
Annie Wahl <pyravice@gmail.com>
2026-07-18 20:03:13 -0600

    I apparently don't understand auth.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
-export { handlers } from "@/auth"
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth/auth"; 
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ lib/auth/auth.ts
@@ -0,0 +1,24 @@
+import type { NextAuthOptions } from "next-auth"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "authelia",
+      name: "Authelia",
+      type: "oauth",
+      issuer: process.env.AUTHELIA_ISSUER, 
+      clientId: process.env.AUTHELIA_CLIENT_ID,
+      clientSecret: process.env.AUTHELIA_CLIENT_SECRET,
+      authorization: {
+        params: { scope: "openid profile email" },
+      },
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name || profile.preferred_username,
+          email: profile.email,
+        }
+      },
+    },
+  ],
+};
